fe998cba7c416a95225a0b1e58cda2ab081325ee
Gilbert <thabogilly@gmail.com>
2023-01-20 09:06:10 +0300

    commits

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import HowManyParks from "./parks/HowManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife, elevation} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  HowManyParks(); // => "42 parks!"
+
+  return (
+    <> 
+     <h1>Colorado State Parks!</h1>
+     <MesaVerde />
+     {trees}
+
+    </>
+
+  )
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks
